b24f0ca89b43605edbeacc1ad3aec454475ce60c
Jay-Kalariya <Jay-kalariya>
2024-09-01 14:38:21 +0530

    new chnages

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Navbar Section
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
